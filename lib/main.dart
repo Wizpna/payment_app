9c3f042add33a3f0774c62e4c31975c857f95922
Amadi Promise <wizpna@gmail.com>
2019-08-25 11:04:14 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rave/flutter_rave.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.orangeAccent,
+      theme: ThemeData(primaryColor: Colors.orangeAccent),
+      title: 'Flutterwave',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Flutterwave'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Builder(
+          builder: (context) => SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10),
+              child: InkWell(
+                onTap: () => _pay(context),
+                child: Card(
+                  color: Colors.orangeAccent,
+                  elevation: 15,
+                  child: Container(
+                    height: 250,
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "Card Payment",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Icon(
+                            Icons.payment,
+                            color: Colors.black,
+                            size: 30,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _pay(BuildContext context) {
+    final snackBar_onFailure = SnackBar(content: Text('Transaction failed'));
+    final snackBar_onClosed = SnackBar(content: Text('Transaction closed'));
+    final _rave = RaveCardPayment(
+      isDemo: true,
+      encKey: "1b40200e3137f43b3e50c0ba",
+      publicKey: "FLWPUBK-67f9dc5a446d63ff6ad9b5706531103a-X",
+      transactionRef: "hvHPvKYaRuJLlJWSPWGGKUyaAfWeZKnm",
+      amount: 100,
+      email: "demo1@example.com",
+      onSuccess: (response) {
+        print("$response");
+        print("Transaction Successful");
+        if (mounted) {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Transaction Sucessful!"),
+              backgroundColor: Colors.green,
+              duration: Duration(
+                seconds: 5,
+              ),
+            ),
+          );
+        }
+      },
+      onFailure: (err) {
+        print("$err");
+        print("Transaction failed");
+        Scaffold.of(context).showSnackBar(snackBar_onFailure);
+      },
+      onClosed: () {
+        print("Transaction closed");
+        Scaffold.of(context).showSnackBar(snackBar_onClosed);
+      },
+      context: context,
+    );
+    _rave.process();
+  }
+}
